abcbfc4c1f45e0e3d3390ad35e5abdee0115d559
Salih Zioui <www.cjb@gmx.de>
2024-12-31 22:01:43 +0100

    fix: downgrade vite to be compatible with plugins and add absolute import support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 	build: {
 		sourcemap: true,
 		rollupOptions: {
-			external: ["tests/**/*"],
+			external: ["tests/unit/**/*"],
 		},
 	},
 	test: {
