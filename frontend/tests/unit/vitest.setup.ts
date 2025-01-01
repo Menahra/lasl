8505a8600648e1603f54a93140bc95bef291096c
Salih Zioui <www.cjb@gmx.de>
2025-01-01 22:35:19 +0100

    feat: lint test files as well on frontend project

--- frontend/tests/unit/vitest.setup.ts
+++ frontend/tests/unit/vitest.setup.ts
@@ -1,12 +1,11 @@
-import * as matchers from '@testing-library/jest-dom/matchers';
-import { cleanup } from '@testing-library/react';
-import { afterEach, expect } from 'vitest';
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { cleanup } from "@testing-library/react";
+import { afterEach, expect } from "vitest";
 
 // since we do not use vitest globals we need to extend expect
 expect.extend(matchers);
 
 // clear environment after each test case
 afterEach(() => {
-  cleanup();
+	cleanup();
 });
-
