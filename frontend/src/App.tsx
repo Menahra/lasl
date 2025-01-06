7259a244fddd26b402ab909d42ee5ca3cf6af464
Salih Zioui <www.cjb@gmx.de>
2025-01-06 22:19:57 +0100

    chore: correctly format files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import { InputField } from "./components/input-field/InputField";
 import "./styles/_global.scss";
 
 export const App = () => (
-  <InputField
-    onInputValueChange={(newValue) => console.log(newValue)}
-    placeholder="Test123"
-    label="Hello is an حَرْفُ الْنَصْبٌ with four properties"
-  />
+	<InputField
+		onInputValueChange={(newValue) => console.log(newValue)}
+		placeholder="Test123"
+		label="Hello is an حَرْفُ الْنَصْبٌ with four properties"
+	/>
 );
--- frontend/src/components/input-field/InputField.tsx
+++ frontend/src/components/input-field/InputField.tsx
@@ -3,49 +3,49 @@ import { v4 as uuidV4 } from "uuid";
 import "./styles.scss";
 
 type SearchFieldProps = {
-  /**
-   * the placeholder which is shown if no value is set
-   * provide undefined if you do not want to show anything in case of an empty value
-   */
-  placeholder?: string;
+	/**
+	 * the placeholder which is shown if no value is set
+	 * provide undefined if you do not want to show anything in case of an empty value
+	 */
+	placeholder?: string;
 
-  /** the (accesible) description for this input */
-  label: string;
+	/** the (accesible) description for this input */
+	label: string;
 
-  /** when this toggle is set to true the label will only be shown for screenreaders */
-  showLabel?: boolean;
+	/** when this toggle is set to true the label will only be shown for screenreaders */
+	showLabel?: boolean;
 
-  /** the callback to be executed when ever the value of this input changes */
-  onInputValueChange: (searchValue: string) => void;
+	/** the callback to be executed when ever the value of this input changes */
+	onInputValueChange: (searchValue: string) => void;
 };
 
 export const InputField = ({
-  placeholder,
-  label,
-  showLabel,
-  onInputValueChange,
+	placeholder,
+	label,
+	showLabel,
+	onInputValueChange,
 }: SearchFieldProps) => {
-  const inputId = uuidV4();
-  const handleInputFieldValueChange = (
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    onInputValueChange(event.target.value);
-  };
+	const inputId = uuidV4();
+	const handleInputFieldValueChange = (
+		event: ChangeEvent<HTMLInputElement>,
+	) => {
+		onInputValueChange(event.target.value);
+	};
 
-  return (
-    <div className="InputField">
-      <label
-        htmlFor={inputId}
-        className={`InputFieldLabel ${showLabel ? "" : "sr-only"}`}
-      >
-        {label}
-      </label>
-      <input
-        id={inputId}
-        className="InputFieldInput"
-        onChange={handleInputFieldValueChange}
-        placeholder={placeholder}
-      />
-    </div>
-  );
+	return (
+		<div className="InputField">
+			<label
+				htmlFor={inputId}
+				className={`InputFieldLabel ${showLabel ? "" : "sr-only"}`}
+			>
+				{label}
+			</label>
+			<input
+				id={inputId}
+				className="InputFieldInput"
+				onChange={handleInputFieldValueChange}
+				placeholder={placeholder}
+			/>
+		</div>
+	);
 };
