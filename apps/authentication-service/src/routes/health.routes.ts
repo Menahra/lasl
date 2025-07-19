5ee0ef88c33123d49bda486814b0390cec4208a2
Salih Zioui <www.cjb@gmx.de>
2025-07-19 19:46:01 +0200

    fix formatting

--- apps/authentication-service/src/routes/health.routes.ts
+++ apps/authentication-service/src/routes/health.routes.ts
@@ -3,12 +3,10 @@ import { StatusCodes } from "http-status-codes";
 
 export const healthRoutes = (fastifyInstance: FastifyInstance) => {
   fastifyInstance.get("/healthcheck", (_request, reply) => {
-    reply
-      .code(StatusCodes.OK)
-      .send({
-        status: "ok",
-        message: "Authentication service is running and healthy!",
-        uptime: process.uptime(),
-      });
+    reply.code(StatusCodes.OK).send({
+      status: "ok",
+      message: "Authentication service is running and healthy!",
+      uptime: process.uptime(),
+    });
   });
 };
