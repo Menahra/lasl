8a0c551b6d1bb67c2fc3137c6723f0e30a0e6325
Salih Zioui <www.cjb@gmx.de>
2026-01-03 16:56:10 +0100

    chore: add more auth related tests

--- packages/e2e-tests/tests/auth/login.spec.ts
+++ packages/e2e-tests/tests/auth/login.spec.ts
@@ -2,14 +2,13 @@ import { authRoutes } from "@lasl/app-contracts/routes/auth";
 import { expect, test } from "@playwright/test";
 
 test.describe("User Login", () => {
-  const user = {
-    firstName: "John",
-    lastName: "Doe",
-    email: `test-${Date.now()}@example.com`,
-    password: "SecurePassword123!",
-  } as const;
-
   test("cannot login without verification of email", async ({ page }) => {
+    const user = {
+      firstName: "John",
+      lastName: "Doe",
+      email: `test-${Date.now()}-${Math.random()}@example.com`,
+      password: "SecurePassword123!",
+    } as const;
     await page.goto(authRoutes.register);
 
     await page
@@ -32,6 +31,43 @@ test.describe("User Login", () => {
     await page
       .getByRole("textbox", { name: /^password$/i })
       .fill(user.password);
+
+    await page.getByRole("button", { name: /sign in/i }).click();
+
+    await expect(page.getByText(/verify your email first/i)).toBeVisible();
+  });
+
+  test("unverified user can also not login with other password", async ({
+    page,
+  }) => {
+    const user = {
+      firstName: "John",
+      lastName: "Doe",
+      email: `test-${Date.now()}-${Math.random()}@example.com`,
+      password: "SecurePassword123!",
+    } as const;
+    await page.goto(authRoutes.register);
+
+    await page
+      .getByRole("textbox", { name: /first name/i })
+      .fill(user.firstName);
+    await page.getByRole("textbox", { name: /last name/i }).fill(user.lastName);
+    await page.getByRole("textbox", { name: /email/i }).fill(user.email);
+    await page
+      .getByRole("textbox", { name: /^password$/i })
+      .fill(user.password);
+    await page
+      .getByRole("textbox", { name: /confirm password/i })
+      .fill(user.password);
+    await page.getByRole("button", { name: /create account/i }).click();
+
+    await expect(page).toHaveURL(authRoutes.registerSuccess);
+    await page.goto(authRoutes.login);
+
+    await page.getByRole("textbox", { name: /email/i }).fill(user.email);
+    await page
+      .getByRole("textbox", { name: /^password$/i })
+      .fill(`${user.password}1`);
 
     await page.getByRole("button", { name: /sign in/i }).click();
 
--- packages/e2e-tests/tests/auth/registration.spec.ts
+++ packages/e2e-tests/tests/auth/registration.spec.ts
@@ -42,4 +42,49 @@ test.describe("User Registration", () => {
 
     await expect(submit).toBeDisabled();
   });
+
+  test("cannot register the same email twice", async ({ page }) => {
+    const user = {
+      firstName: "John",
+      lastName: "Doe",
+      email: `test-${Date.now()}@example.com`,
+      password: "SecurePassword123!",
+    } as const;
+
+    await page.goto(authRoutes.register);
+
+    await page
+      .getByRole("textbox", { name: /first name/i })
+      .fill(user.firstName);
+    await page.getByRole("textbox", { name: /last name/i }).fill(user.lastName);
+    await page.getByRole("textbox", { name: /email/i }).fill(user.email);
+    await page
+      .getByRole("textbox", { name: /^password$/i })
+      .fill(user.password);
+    await page
+      .getByRole("textbox", { name: /confirm password/i })
+      .fill(user.password);
+    await page.getByRole("button", { name: /create account/i }).click();
+
+    await expect(page.locator("text=Check your email")).toBeVisible();
+
+    await expect(page).toHaveURL(authRoutes.registerSuccess);
+    await page.goto(authRoutes.register);
+
+    await page
+      .getByRole("textbox", { name: /first name/i })
+      .fill(user.firstName);
+    await page.getByRole("textbox", { name: /last name/i }).fill(user.lastName);
+    await page.getByRole("textbox", { name: /email/i }).fill(user.email);
+    await page
+      .getByRole("textbox", { name: /^password$/i })
+      .fill(user.password);
+    await page
+      .getByRole("textbox", { name: /confirm password/i })
+      .fill(user.password);
+    await page.getByRole("button", { name: /create account/i }).click();
+
+    expect(page).not.toHaveURL(authRoutes.registerSuccess);
+    await expect(page.getByText(/already registered/i)).toBeVisible();
+  });
 });
