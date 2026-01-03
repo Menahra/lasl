d01f143a37773abef38db6a1be50de360427c729
Salih Zioui <www.cjb@gmx.de>
2026-01-03 13:05:58 +0100

    chore: create reusable user type

--- packages/app-contracts/src/schemas/user.schemas.ts
+++ packages/app-contracts/src/schemas/user.schemas.ts
@@ -63,3 +63,8 @@ export const resetPasswordParamsSchema = z.object({
   id: z.string(),
   passwordResetCode: z.string(),
 });
+
+export const createAuthUserSchema = createUserSchema.omit({
+  passwordConfirmation: true,
+});
+export type CreateAuthUser = z.infer<typeof createAuthUserSchema>;
