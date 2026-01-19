a147c7c4f0d165503c5c820b35bb3416ffd3b38a
Salih Zioui <www.cjb@gmx.de>
2026-01-19 18:50:42 +0100

    chore: fix formatting issue

--- apps/frontend/src/app/routes/_auth/register/verify/$id/$verificationCode/index.tsx
+++ apps/frontend/src/app/routes/_auth/register/verify/$id/$verificationCode/index.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { RegisterVerifyPage } from "@/src/app/pages/register/register-verify-page/RegisterVerifyPage.tsx";
 
-// biome-ignore lint/security/noSecrets: route name, not a secret
-export const Route = createFileRoute("/_auth/register/verify/$id/$verificationCode/")(
-  {
-    component: RegisterVerifyPage,
-  },
-);
+export const Route = createFileRoute(
+  // biome-ignore lint/security/noSecrets: route name, not a secret
+  "/_auth/register/verify/$id/$verificationCode/",
+)({
+  component: RegisterVerifyPage,
+});
