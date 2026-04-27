3fe8f2046d8982022177b9703a5b41e588e8197d
Stefan Zioui <zioui@bikeleasing.de>
2026-04-27 12:30:09 +0200

    chore: remove unused directive

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -20,7 +20,6 @@ export default defineConfig(() => {
         routesDirectory: "src/app/routes",
       }),
       react(),
-      //@ts-expect-error silencing this one for now
       babel({
         plugins: ["@lingui/babel-plugin-lingui-macro"],
       }),
